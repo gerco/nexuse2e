04d7b98db6f73664447fd683187b52e3d21eba14
mbreilmann <mbreilmann@0f8715a3-b92c-0410-9248-d6441137b268>
2007-07-25 16:31:04 +0000

    Updated version
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@156 0f8715a3-b92c-0410-9248-d6441137b268

--- src/main/java/org/nexuse2e/Version.java
+++ src/main/java/org/nexuse2e/Version.java
@@ -21,13 +21,13 @@ package org.nexuse2e;
 
 public class Version {
 
-    private static final String  SUBVERSION_REVISION = "141";
-    private static final String  SUBVERSION_DATE     = "2007/07/19 11:21:44";
+    private static final String  SUBVERSION_REVISION = "155";
+    private static final String  SUBVERSION_DATE     = "2007/07/25 18:21:55";
     private static final boolean BETA                = false;
     private static final int     BETA_VERSION        = 5;
     private static final boolean RC                  = false;
     private static final int     RC_VERSION          = 1;
-    private static final String  VERSION             = "4.0.4" + ( BETA ? " BETA-" + BETA_VERSION :  ( RC ? " RC-" + RC_VERSION : "" ) ) + ", Build "
+    private static final String  VERSION             = "4.0.5" + ( BETA ? " BETA-" + BETA_VERSION :  ( RC ? " RC-" + RC_VERSION : "" ) ) + ", Build "
                                                              + SUBVERSION_REVISION + " (" + SUBVERSION_DATE + ")";
 
     public static String getVersion() {
