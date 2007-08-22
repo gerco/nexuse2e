8f8ace756f30b78ec0b91688007dae5725d00aab
j_reese <j_reese@0f8715a3-b92c-0410-9248-d6441137b268>
2007-08-22 19:13:06 +0000

    Web Service adapter for backend delivery
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@226 0f8715a3-b92c-0410-9248-d6441137b268

---
+++ src/main/java/org/nexuse2e/integration/ProcessInboundMessageException.java
@@ -0,0 +1,66 @@
+/**
+ * NEXUSe2e Business Messaging Open Source  
+ * Copyright 2007, Tamgroup and X-ioma GmbH   
+ *  
+ * This is free software; you can redistribute it and/or modify it  
+ * under the terms of the GNU Lesser General Public License as  
+ * published by the Free Software Foundation version 2.1 of  
+ * the License.  
+ *  
+ * This software is distributed in the hope that it will be useful,  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  
+ * Lesser General Public License for more details.  
+ *  
+ * You should have received a copy of the GNU Lesser General Public  
+ * License along with this software; if not, write to the Free  
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.nexuse2e.integration;
+
+/**
+ * Indicates a processing error on an inbound message.
+ * @author jonas.reese
+ */
+public class ProcessInboundMessageException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    
+    private int errorCode;
+    private String errorDescription;
+
+    public ProcessInboundMessageException() {
+        super();
+    }
+
+    public ProcessInboundMessageException( String message, Throwable cause ) {
+        super( message, cause );
+    }
+
+    public ProcessInboundMessageException( String message ) {
+        super( message );
+    }
+
+    public ProcessInboundMessageException( Throwable cause ) {
+        super(cause);
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public void setErrorDescription( String errorDescription ) {
+        this.errorDescription = errorDescription;
+    }
+
+    public String getErrorDescription() {
+        return errorDescription;
+    }
+}
