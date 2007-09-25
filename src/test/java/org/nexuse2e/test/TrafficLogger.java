cc573b6ca680fd96492c8040322a3f090a4c24f1
j_reese <j_reese@0f8715a3-b92c-0410-9248-d6441137b268>
2007-09-25 12:39:23 +0000

    Helper class that prints out request
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@267 0f8715a3-b92c-0410-9248-d6441137b268

---
+++ src/test/java/org/nexuse2e/test/TrafficLogger.java
@@ -0,0 +1,46 @@
+package org.nexuse2e.test;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * Created: 25.09.2007
+ * TODO Class documentation
+ *
+ * @author jonas.reese
+ * @version $LastChangedRevision$ - $LastChangedDate$ by $LastChangedBy$
+ */
+public class TrafficLogger implements Controller, ApplicationContextAware {
+
+    public ModelAndView handleRequest( HttpServletRequest request, HttpServletResponse response ) {
+        try {
+            List<?> l = IOUtils.readLines( request.getInputStream() );
+            for (Object s : l) {
+                System.out.println( s );
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.ApplicationContext)
+     */
+    public void setApplicationContext( ApplicationContext context )
+            throws BeansException {
+        Object o = context.getBean( "xfire" );
+        System.out.println( o );
+    }
+}
