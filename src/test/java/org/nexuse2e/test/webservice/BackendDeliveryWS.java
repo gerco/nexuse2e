a7c14a269594cefe061e7482abd01b37c9a63d39
j_reese <j_reese@0f8715a3-b92c-0410-9248-d6441137b268>
2007-09-25 12:42:46 +0000

    Changed interface
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@269 0f8715a3-b92c-0410-9248-d6441137b268

--- src/test/java/org/nexuse2e/test/webservice/BackendDeliveryWS.java
+++ src/test/java/org/nexuse2e/test/webservice/BackendDeliveryWS.java
@@ -19,8 +19,12 @@ public class BackendDeliveryWS implements BackendDeliveryInterface {
      * @see org.nexuse2e.integration.BackendDeliveryInterface#processInboundMessage(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String[])
      */
     public String processInboundMessage(
-            String choreographyId, String businessPartnerId, String actionId,
-            String conversationId, String messageId, String[] payload ) throws ProcessInboundMessageException {
+            String choreographyId,
+            String businessPartnerId,
+            String actionId,
+            String conversationId,
+            String messageId,
+            String payload) throws ProcessInboundMessageException {
         LOG.info( "choreographyId=" + choreographyId + " businessPartnerId="
                 + businessPartnerId + " actionId=" + actionId
                 + " conversationId=" + conversationId + " messageId=" + messageId + " payload=" + payload );
@@ -32,9 +36,13 @@ public class BackendDeliveryWS implements BackendDeliveryInterface {
  * Add this to the NexusWebServiceDispatcher-servlet.xml bean configuration:
  * 
     <!-- BEGIN TESTING SECTION -->
+    <bean id="xfire.annotServiceFactory"
+      class="org.codehaus.xfire.annotations.AnnotationServiceFactory"
+      singleton="true">
+    </bean>
     <bean id="BackendDeliveryInterface" class="org.codehaus.xfire.spring.remoting.XFireExporter">
         <property name="serviceFactory">
-            <ref bean="xfire.serviceFactory"/>
+            <ref bean="xfire.annotServiceFactory"/>
         </property>
         <property name="xfire">
             <ref bean="xfire"/>
@@ -55,5 +63,10 @@ public class BackendDeliveryWS implements BackendDeliveryInterface {
         <ref bean="BackendDeliveryInterface"/>
     </entry>
  * 
+ * For client testing against the output logger, add this to URL handler mapping:
+ * 
+    <entry key="/RequestLogger">
+        <bean class="org.nexuse2e.test.RequestLogger"/>
+    </entry>
  */
 }
