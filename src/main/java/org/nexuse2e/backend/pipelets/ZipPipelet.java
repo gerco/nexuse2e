e991145b431c5a9dabca2c6e8280002b1a19227d
mbreilmann <mbreilmann@0f8715a3-b92c-0410-9248-d6441137b268>
2007-10-19 08:06:33 +0000

    Added file name configuration parameters
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@279 0f8715a3-b92c-0410-9248-d6441137b268

--- src/main/java/org/nexuse2e/backend/pipelets/ZipPipelet.java
+++ src/main/java/org/nexuse2e/backend/pipelets/ZipPipelet.java
@@ -24,12 +24,18 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import org.nexuse2e.Engine;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.nexuse2e.NexusException;
+import org.nexuse2e.configuration.EngineConfiguration;
+import org.nexuse2e.configuration.ParameterDescriptor;
+import org.nexuse2e.configuration.Constants.ParameterType;
 import org.nexuse2e.messaging.AbstractPipelet;
 import org.nexuse2e.messaging.MessageContext;
 import org.nexuse2e.pojo.ConversationPojo;
@@ -43,6 +49,63 @@ import org.nexuse2e.pojo.PartnerPojo;
  */
 public class ZipPipelet extends AbstractPipelet {
 
+    private static Logger      LOG               = Logger.getLogger( ZipPipelet.class );
+
+    public final static String FILE_NAME_PATTERN = "fileNamePattern";
+    public final static String TIMESTAMP_PATTERN = "timestampPattern";
+    public final static String FILE_EXTENSION    = "fileExtension";
+
+    private String             fileNamePattern   = null;
+    private String             timestampPattern  = null;
+    private String             fileExtension     = null;
+
+    public ZipPipelet() {
+
+        parameterMap.put( FILE_NAME_PATTERN, new ParameterDescriptor( ParameterType.STRING, "File name pattern",
+                "The file name pattern to use for payloads", "Payload_${sequence}" ) );
+        parameterMap.put( FILE_EXTENSION, new ParameterDescriptor( ParameterType.STRING, "File extension",
+                "The file extension to append", "Payloads" ) );
+        parameterMap.put( TIMESTAMP_PATTERN, new ParameterDescriptor( ParameterType.STRING, "Timestamp pattern",
+                "The timestamp pattern to use", "yyyy-MM-dd HH.mm.ssSSS" ) );
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.nexuse2e.messaging.AbstractPipelet#initialize(org.nexuse2e.configuration.EngineConfiguration)
+     */
+    @Override
+    public void initialize( EngineConfiguration config ) throws InstantiationException {
+
+        LOG.trace( "initializing" );
+
+        String tempParam = getParameter( FILE_NAME_PATTERN );
+        if ( ( tempParam != null ) && ( tempParam.length() != 0 ) ) {
+            fileNamePattern = tempParam;
+        } else {
+            LOG.error( "No file name pattern provided!" );
+            fileNamePattern = "Payload_${sequence}";
+        }
+
+        tempParam = getParameter( FILE_EXTENSION );
+        if ( ( tempParam != null ) && ( tempParam.length() != 0 ) ) {
+            fileExtension = tempParam;
+            if ( !fileExtension.startsWith( "." ) ) {
+                fileExtension = "." + fileExtension;
+            }
+        } else {
+            LOG.error( "No file extension provided!" );
+            fileExtension = ".dat";
+        }
+
+        tempParam = getParameter( TIMESTAMP_PATTERN );
+        if ( ( tempParam != null ) && ( tempParam.length() != 0 ) ) {
+            timestampPattern = tempParam;
+        } else {
+            LOG.error( "No timestamp pattern provided!" );
+            timestampPattern = "yyyy-MM-dd HH.mm.ssSSS";
+        }
+    }
+
     /* (non-Javadoc)
      * @see org.nexuse2e.messaging.Pipelet#processMessage(org.nexuse2e.messaging.MessageContext)
      */
@@ -54,18 +117,32 @@ public class ZipPipelet extends AbstractPipelet {
             byte[] newContent = null;
             String mimeType = "application/x-zip-compressed";
 
+            int sequence = 1;
+
             MessagePojo messagePojo = messageContext.getMessagePojo();
             for ( MessagePayloadPojo messagePayloadPojo : messagePojo.getMessagePayloads() ) {
                 try {
                     ByteArrayOutputStream baos = new ByteArrayOutputStream();
                     ZipOutputStream zos = new ZipOutputStream( baos );
                     zos.setMethod( ZipOutputStream.DEFLATED );
-                    String fileExtension = ".txt";
-                    ZipEntry zipEntry = new ZipEntry( messagePayloadPojo.getContentId() + fileExtension );
+
+                    String filename = null;
+
+                    SimpleDateFormat sdt = new SimpleDateFormat( timestampPattern );
+
+                    filename = StringUtils.replace( fileNamePattern, "${timestamp}", sdt.format( new Date() ) );
+
+                    filename = StringUtils.replace( filename, "${sequence}", "" + sequence );
+
+                    sequence++;
+
+                    filename += fileExtension;
+
+                    ZipEntry zipEntry = new ZipEntry( filename );
                     zipEntry.setSize( messagePayloadPojo.getPayloadData().length );
                     if ( messagePayloadPojo.getMimeType() != null ) {
-                    zipEntry.setComment( messagePayloadPojo.getMimeType() );
-                    zipEntry.setExtra( messagePayloadPojo.getMimeType().getBytes() );
+                        zipEntry.setComment( messagePayloadPojo.getMimeType() );
+                        zipEntry.setExtra( messagePayloadPojo.getMimeType().getBytes() );
                     }
                     zos.putNextEntry( zipEntry );
                     zos.write( messagePayloadPojo.getPayloadData() );
@@ -123,7 +200,15 @@ public class ZipPipelet extends AbstractPipelet {
             messagePayloadPojo.setMessage( messagePojo );
             messagePojo.getMessagePayloads().add( messagePayloadPojo );
 
-            new ZipPipelet().processMessage( messageContext );
+            ZipPipelet zipPipelet = new ZipPipelet();
+            
+            zipPipelet.setParameter( FILE_NAME_PATTERN, "Payload_${sequence}_${timestamp}" );
+            zipPipelet.setParameter( TIMESTAMP_PATTERN, "yyyyMMddHHmmssSSS" );
+            zipPipelet.setParameter( FILE_EXTENSION, "xml" );
+            
+            zipPipelet.initialize( null );
+            
+            zipPipelet.processMessage( messageContext );
 
             for ( Iterator<MessagePayloadPojo> payloads = messageContext.getMessagePojo().getMessagePayloads()
                     .iterator(); payloads.hasNext(); ) {
