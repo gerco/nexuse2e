1fb9b5a7abe431b1ba3e4e654b9630a1bebc58f3
gesch <gesch@0f8715a3-b92c-0410-9248-d6441137b268>
2008-01-16 16:04:47 +0000

    parameter replacement added for flexible schema validation in frontend and backend.
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@412 0f8715a3-b92c-0410-9248-d6441137b268

--- src/main/java/org/nexuse2e/backend/pipelets/XmlSchemaValidationPipelet.java
+++ src/main/java/org/nexuse2e/backend/pipelets/XmlSchemaValidationPipelet.java
@@ -21,7 +21,9 @@ package org.nexuse2e.backend.pipelets;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
@@ -40,6 +42,7 @@ import org.nexuse2e.configuration.Constants.ParameterType;
 import org.nexuse2e.messaging.AbstractPipelet;
 import org.nexuse2e.messaging.MessageContext;
 import org.nexuse2e.pojo.MessagePayloadPojo;
+import org.nexuse2e.util.ServerPropertiesUtil;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 
@@ -49,18 +52,21 @@ import org.xml.sax.InputSource;
  */
 public class XmlSchemaValidationPipelet extends AbstractPipelet {
 
-    private static Logger LOG = Logger.getLogger( XmlSchemaValidationPipelet.class );
-    
-    protected static final String SCHEMA_FILE_PATH = "schemaFilePath";
-    
-    protected String schemaFilePath;
-    protected Validator validator = null;
-    
+    private static Logger            LOG              = Logger.getLogger( XmlSchemaValidationPipelet.class );
+
+    protected static final String    SCHEMA_FILE_PATH = "schemaFilePath";
+
+    protected String                 schemaFilePath;
+    protected Map<String, Validator> validators       = new HashMap<String, Validator>();
+
     public XmlSchemaValidationPipelet() {
+
+        frontendPipelet = false;
+
         parameterMap.put( SCHEMA_FILE_PATH, new ParameterDescriptor( ParameterType.STRING, "Schema file",
                 "Path to the schema file for XML validation", "" ) );
     }
-    
+
     /* (non-Javadoc)
      * @see org.nexuse2e.messaging.AbstractPipelet#initialize(org.nexuse2e.configuration.EngineConfiguration)
      */
@@ -69,27 +75,22 @@ public class XmlSchemaValidationPipelet extends AbstractPipelet {
 
         LOG.trace( "initializing" );
         schemaFilePath = getParameter( SCHEMA_FILE_PATH );
-        
-        if ( !StringUtils.isEmpty( schemaFilePath ) ) {
-            SchemaFactory factory = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
-            Schema schema;
-            try {
-                schema = factory.newSchema( new File( schemaFilePath ) );
-                validator = schema.newValidator();
-                LOG.trace( "Using XML Schema: " + schemaFilePath );
-            } catch ( Exception e ) {
-//                LOG.error( "Error reading XML Schema file: " + schemaFilePath );
-                throw new InstantiationException( "Error reading XML Schema file: " + schemaFilePath
-                        + ". Cause: " + e.getMessage() );
-            }
-        } else {
-//            LOG.error( "No XML Schema file provided" );
-            throw new InstantiationException( "No XML Schema file provided" );
-        }
+        schemaFilePath = ServerPropertiesUtil.replacePathSeparators( schemaFilePath );
+
         super.initialize( config );
     }
-    
-    
+
+    /* (non-Javadoc)
+     * @see org.nexuse2e.messaging.AbstractPipelet#teardown()
+     */
+    @Override
+    public void teardown() {
+
+        validators = new HashMap<String, Validator>();
+
+        super.teardown();
+    }
+
     /* (non-Javadoc)
      * @see org.nexuse2e.messaging.AbstractPipelet#processMessage(org.nexuse2e.messaging.MessageContext)
      */
@@ -97,33 +98,51 @@ public class XmlSchemaValidationPipelet extends AbstractPipelet {
     public MessageContext processMessage( MessageContext messageContext ) throws IllegalArgumentException,
             IllegalStateException, NexusException {
 
-        if ( validator != null ) {
+        if ( !StringUtils.isEmpty( schemaFilePath ) ) {
             // get message payloads
             List<MessagePayloadPojo> payloads = messageContext.getMessagePojo().getMessagePayloads();
-            
+
             // iterate over message payloads
+
             for ( MessagePayloadPojo pojo : payloads ) {
-                
+
+                String tempFilePath = ServerPropertiesUtil.replaceServerProperties( schemaFilePath, messageContext );
+                tempFilePath = ServerPropertiesUtil.replacePayloadDependedValues( tempFilePath, pojo
+                        .getSequenceNumber(), messageContext );
+
+                Validator validator = validators.get( tempFilePath );
+
+                if ( validator == null ) {
+                    SchemaFactory factory = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
+                    Schema schema;
+                    try {
+                        schema = factory.newSchema( new File( tempFilePath ) );
+                        validator = schema.newValidator();
+                        LOG.debug( "Using XML Schema: " + tempFilePath );
+                        validators.put( tempFilePath, validator );
+                    } catch ( Exception e ) {
+                        throw new NexusException( "Error reading XML Schema file: " + tempFilePath + ". Cause: "
+                                + e.getMessage() );
+                    }
+                }
+
                 try {
                     InputSource inputSource = new InputSource( new ByteArrayInputStream( pojo.getPayloadData() ) );
                     DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
                     documentBuilderFactory.setNamespaceAware( true );
                     DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
                     Document document = builder.parse( inputSource );
-        
+
                     validator.validate( new DOMSource( document ) );
-        
+
                     LOG.info( "Validated XML Schema compliance of payload - OK!" );
                 } catch ( Exception e ) {
                     throw new NexusException( "XML validation error (Conversation Id: "
-                            + messageContext.getMessagePojo().getConversation().getConversationId()
-                            + ", Message Id: " + messageContext.getMessagePojo().getMessageId() + "): "
-                            + e.getMessage(), e );
+                            + messageContext.getMessagePojo().getConversation().getConversationId() + ", Message Id: "
+                            + messageContext.getMessagePojo().getMessageId() + "): " + e.getMessage(), e );
                 }
             }
-        } else {
-            throw new NexusException( "Validator cannot not be initialized" );
-        }
+        } 
 
         return messageContext;
     }
