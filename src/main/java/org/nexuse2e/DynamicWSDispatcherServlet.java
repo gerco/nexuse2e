fe9781aae5d910bbc5186db66f5048bd27720c9c
j_reese <j_reese@0f8715a3-b92c-0410-9248-d6441137b268>
2008-01-30 15:00:46 +0000

    CXF stuff
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@455 0f8715a3-b92c-0410-9248-d6441137b268

---
+++ src/main/java/org/nexuse2e/DynamicWSDispatcherServlet.java
@@ -0,0 +1,56 @@
+package org.nexuse2e;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
+import org.apache.log4j.Logger;
+
+/**
+ * This subclass of <code>CXFNonSpringServlet</code> keeps a reference on the instance
+ * created by the servlet container in order to make it available for code that
+ * dynamically registers/unregisters web services.
+ *
+ * @author Jonas Reese
+ * @version $LastChangedRevision:  $ - $LastChangedDate:  $ by $LastChangedBy:  $
+ */
+public class DynamicWSDispatcherServlet extends CXFNonSpringServlet {
+    private static Logger      LOG                         = Logger.getLogger( DynamicWSDispatcherServlet.class );
+    
+    
+    private static final long serialVersionUID = 1L;
+    
+    private static DynamicWSDispatcherServlet instance;
+    
+    private ServletConfig servletConfig;
+
+    public DynamicWSDispatcherServlet() {
+        instance = this;
+    }
+    
+    public void init( ServletConfig servletConfig ) throws ServletException {
+        this.servletConfig = servletConfig;
+        super.init( servletConfig );
+        
+        Bus bus = DynamicWSDispatcherServlet.getInstance().getBus();
+        if (bus != null) {
+            BusFactory.setDefaultBus( bus );
+        } else {
+            LOG.error( "CXF bus is null" );
+        }
+    }
+    
+    public void reinitialize() {
+        controller = createServletController( servletConfig );
+    }
+
+    /**
+     * Gets the most recently created instance of this class.
+     * @return The last created instance, or <code>null</code> if none was created yet.
+     */
+    public static DynamicWSDispatcherServlet getInstance() {
+        return instance;
+    }
+}
