d34b0ee298cbe41cde70186418f92f5656a29905
j_reese <j_reese@0f8715a3-b92c-0410-9248-d6441137b268>
2008-02-11 15:52:02 +0000

    Added headers
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@500 0f8715a3-b92c-0410-9248-d6441137b268

--- src/main/java/org/nexuse2e/reporting/BirtEngine.java
+++ src/main/java/org/nexuse2e/reporting/BirtEngine.java
@@ -1,3 +1,22 @@
+/**
+ * NEXUSe2e Business Messaging Open Source  
+ * Copyright 2007, Tamgroup and X-ioma GmbH   
+ *  
+ * This is free software; you can redistribute it and/or modify it  
+ * under the terms of the GNU Lesser General Public License as  
+ * published by the Free Software Foundation version 2.1 of  
+ * the License.  
+ *  
+ * This software is distributed in the hope that it will be useful,  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  
+ * Lesser General Public License for more details.  
+ *  
+ * You should have received a copy of the GNU Lesser General Public  
+ * License along with this software; if not, write to the Free  
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.nexuse2e.reporting;
 
 
@@ -15,6 +34,13 @@ import org.eclipse.birt.core.framework.Platform;
 import org.eclipse.birt.core.exception.BirtException;
 import org.eclipse.birt.report.engine.api.IReportEngineFactory;
 
+
+/**
+ * Wrapper for the BIRT engine. This class was taken from a sample from Jason Weathersby
+ * (available under http://www.onjava.com/pub/a/onjava/2006/07/26/deploying-birt.html).
+ * 
+ * @author Jonas Reese
+ */
 public class BirtEngine {
 
 	private static IReportEngine birtEngine = null;
@@ -24,14 +50,13 @@ public class BirtEngine {
 	private final static String configFile = "org/nexuse2e/reporting/BirtConfig.properties";
 
 	public static synchronized void initBirtConfig() {
-		loadEngineProps();
+		loadEngineProperties();
 	}
 
-	public static synchronized IReportEngine getBirtEngine(ServletContext sc) throws UnsupportedOperationException {
-		if (birtEngine == null) 
-		{
+	public static synchronized IReportEngine getBirtEngine( ServletContext sc ) {
+		if (birtEngine == null) {
 			EngineConfig config = new EngineConfig();
-			if( configProps != null){
+			if ( configProps != null){
 				String logLevel = configProps.getProperty("logLevel");
 				Level level = Level.OFF;
 				if ("SEVERE".equalsIgnoreCase(logLevel)) {
@@ -59,7 +84,6 @@ public class BirtEngine {
 			IPlatformContext context = new PlatformServletContext( sc );
 			config.setPlatformContext( context );
 
-
 			try {
 				Platform.startup( config );
 			} catch ( BirtException e ) {
@@ -76,7 +100,7 @@ public class BirtEngine {
 		return birtEngine;
 	}
 
-	public static synchronized void destroyBirtEngine() {
+	public static synchronized void shutdown() {
 		if (birtEngine == null) {
 			return;
 		}		
@@ -84,24 +108,16 @@ public class BirtEngine {
 		birtEngine = null;
 	}
 
-	public Object clone() throws CloneNotSupportedException {
-		throw new CloneNotSupportedException();
-	}
-
-	private static void loadEngineProps() {
+	private static void loadEngineProperties() {
 		try {
-			//Config File must be in classpath
 			ClassLoader cl = Thread.currentThread ().getContextClassLoader();
 			InputStream in = null;
-			in = cl.getResourceAsStream (configFile);
-			configProps.load(in);
+			in = cl.getResourceAsStream( configFile );
+			configProps.load( in );
 			in.close();
-
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 }
