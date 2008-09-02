7f497c2a4f70cc9feab8d09966a9b81926d6d554
s_schulze <s_schulze@0f8715a3-b92c-0410-9248-d6441137b268>
2008-09-02 09:57:26 +0000

    Added the ability to define the MIME type for the RawDataMessagePipelet.
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@696 0f8715a3-b92c-0410-9248-d6441137b268

--- src/main/java/org/nexuse2e/messaging/generic/RawDataMessagePipelet.java
+++ src/main/java/org/nexuse2e/messaging/generic/RawDataMessagePipelet.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.nexuse2e.Constants;
 import org.nexuse2e.Engine;
 import org.nexuse2e.NexusException;
+import org.nexuse2e.configuration.ParameterDescriptor;
+import org.nexuse2e.configuration.Constants.ParameterType;
 import org.nexuse2e.messaging.AbstractPipelet;
 import org.nexuse2e.messaging.MessageContext;
 import org.nexuse2e.pojo.MessagePayloadPojo;
@@ -20,9 +22,13 @@ import org.nexuse2e.pojo.MessagePojo;
  */
 public class RawDataMessagePipelet extends AbstractPipelet {
 
+    private static final String MIME_TYPE_PARAM_NAME = "mimeType";
+    
     public RawDataMessagePipelet() {
         forwardPipelet = true;
         frontendPipelet = true;
+        parameterMap.put( MIME_TYPE_PARAM_NAME, new ParameterDescriptor(
+            ParameterType.STRING, "MIME type", "The MIME type of the payload", "text/plain" ) );
     }
     
     /* (non-Javadoc)
@@ -36,7 +42,7 @@ public class RawDataMessagePipelet extends AbstractPipelet {
         messagePayloadPojo.setMessage( messagePojo );
         messagePayloadPojo.setContentId(
                 Engine.getInstance().getIdGenerator( Constants.ID_GENERATOR_MESSAGE_PAYLOAD ).getId() );
-        messagePayloadPojo.setMimeType( "text/plain" );
+        messagePayloadPojo.setMimeType( (String) getParameter( MIME_TYPE_PARAM_NAME ) );
         messagePayloadPojo.setPayloadData( (byte[]) messageContext.getData() );
         List<MessagePayloadPojo> messagePayloads = new ArrayList<MessagePayloadPojo>( 1 );
         messagePayloads.add( messagePayloadPojo );
