4a5fd5989801af5af4992f8b71677399a9401a36
gesch <gesch@0f8715a3-b92c-0410-9248-d6441137b268>
2009-02-20 18:46:13 +0000

    version changed
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@947 0f8715a3-b92c-0410-9248-d6441137b268

--- src/main/java/org/nexuse2e/Version.java
+++ src/main/java/org/nexuse2e/Version.java
@@ -21,13 +21,13 @@ package org.nexuse2e;
 
 public class Version {
 
-    private static final String  SUBVERSION_REVISION = "911";
-    private static final String  SUBVERSION_DATE     = "2009/02/16 11:00:00";
+    private static final String  SUBVERSION_REVISION = "946";
+    private static final String  SUBVERSION_DATE     = "2009/02/20 19:43:00";
     private static final boolean BETA                = false;
     private static final int     BETA_VERSION        = 1;
     private static final boolean RC                  = false;
     private static final int     RC_VERSION          = 1;
-    private static final String  VERSION             = "4.2.1" + ( BETA ? " BETA-" + BETA_VERSION :  ( RC ? " RC-" + RC_VERSION : "" ) ) + ", Build "
+    private static final String  VERSION             = "4.2.2" + ( BETA ? " BETA-" + BETA_VERSION :  ( RC ? " RC-" + RC_VERSION : "" ) ) + ", Build "
                                                              + SUBVERSION_REVISION + " (" + SUBVERSION_DATE + ")";
 
     public static String getVersion() {
