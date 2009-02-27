5e4a44cf29ab3177166c3db1c511908fbb198bd5
s_schulze <s_schulze@0f8715a3-b92c-0410-9248-d6441137b268>
2009-02-27 11:08:56 +0000

    New implementation of Version.java. It takes the version, revision, build no./date from the MANIFEST.MF. The information is provided by Maven and Hudson.
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@958 0f8715a3-b92c-0410-9248-d6441137b268

--- src/main/java/org/nexuse2e/Version.java
+++ src/main/java/org/nexuse2e/Version.java
@@ -19,25 +19,141 @@
  */
 package org.nexuse2e;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.jar.Manifest;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * This class can read the following main attributes from the META-INF/MANIFEST.MF file
+ * to which this class belongs:
+ * <ul>
+ * 	<li>Application-Name</li>
+ *  <li>Application-Artifact-Id</li>
+ *  <li>Application-Group-Id</li>
+ *  <li>Implementation-Svn-Revision</li>
+ *  <li>Implementation-Version</li>
+ *  <li>Implementation-Build-Date</li>
+ * </ul> 
+ * @author Sebastian Schulze
+ * @date 26.02.2009
+ */
+
 public class Version {
+    
+    private static Manifest cachedManifest;
+    
+    public enum MainAttribute {
+    	ApplicationName( "Application-Name" ),
+    	ApplicationArtifactId( "Application-Artifact-Id" ),
+    	ApplicationGroupId( "Application-Group-Id" ),
+    	ImplementationSvnRevision( "Implementation-Svn-Revision" ),
+    	ImplementationVersion( "Implementation-Version" ),
+    	ImplementationBuildDate( "Implementation-Build-Date" ),
+    	HudsonBuildNumber( "Hudson-Build-Number" ),
+    	HudsonProject( "Hudson-Project" ),
+    	HudsonVersion( "Hudson-Version" );
+    	
+    	private String name;
+    	
+    	MainAttribute( String name ) {
+    		this.name = name;
+    	}
+    	
+    	public String getName() {
+    		return name;
+    	}
+    	
+    }
 
-    private static final String  SUBVERSION_REVISION = "951";
-    private static final String  SUBVERSION_DATE     = "2009/02/25 10:44:00";
-    private static final boolean BETA                = false;
-    private static final int     BETA_VERSION        = 1;
-    private static final boolean RC                  = false;
-    private static final int     RC_VERSION          = 1;
-    private static final String  VERSION             = "4.2.2" + ( BETA ? " BETA-" + BETA_VERSION :  ( RC ? " RC-" + RC_VERSION : "" ) ) + ", Build "
-                                                             + SUBVERSION_REVISION + " (" + SUBVERSION_DATE + ")";
-
+    /**
+     * Returns the official version string for this product.
+     * @return The official version string for this product,
+     *  	   or an empty string, if no version information
+     *         is present.
+     */
     public static String getVersion() {
-
-        return VERSION;
+    	String result = "";
+    	try {
+    		String version = getMainAttribute( MainAttribute.ImplementationVersion );
+    		String revision = getMainAttribute( MainAttribute.ImplementationSvnRevision );
+    		String buildNo = getMainAttribute( MainAttribute.HudsonBuildNumber );
+    		String buildDate = getMainAttribute( MainAttribute.ImplementationBuildDate );
+    		result = ( version != null && version.length() > 0 ? version : "unspecified version" ) + ( revision != null && revision.length() > 0 ? ", revision: " + revision : "" )
+    				 + ", build: " + ( buildNo != null && buildNo.length() > 0 ? buildNo : "unofficial" )
+    				 + ( buildDate != null && buildDate.length() > 0 ? " " + buildDate : "" );
+    	} catch ( IOException e ) {}
+    	
+        return result;
     }
+    
+    /**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println( "NexusE2E Version: " + getVersion() );
+	}
 
-    public static void main( String[] args ) {
+	/**
+	 * Loads the manifest file of the JAR to which the given class belongs to. 
+	 * @param clazz The class of the JAR.
+	 * @return An object representation of the manifest file.
+	 * @throws IOException, if an error occurs while reading the file.
+	 */
+	private static Manifest read( Class<?> clazz ) throws IOException {
+		String path = clazz.getResource( toResourceName( clazz ) )
+				.toString();
+		String manifestPath = extractRoot( path, clazz )
+				+ "/META-INF/MANIFEST.MF";
+		InputStream stream = new URL( manifestPath ).openStream();
+		try {
+			return new Manifest( stream );
+		} finally {
+			IOUtils.closeQuietly( stream );
+		}
+	}
 
-        System.out.println( "NexusE2E Version: " + VERSION );
-    }
+	/*
+	 * Helper method for read()
+	 */
+	private static String extractRoot( String path, Class<?> mainClass ) {
+		if ( path.contains( "!" ) ) {
+			return path.substring( 0, path.lastIndexOf( "!" ) + 1 );
+		} else {
+			return StringUtils.substringBefore( path,
+												mainClass.getName().replace( '.', '/' ) );
+		}
+	}
+
+	/*
+	 * Helper method for read()
+	 */
+	private static String toResourceName(Class<?> mainClass) {
+		return "/" + mainClass.getName().replace('.', '/') + ".class";
+	}
+	
+	/**
+	 * Reads the value of a main attribute of a (cached) version of
+	 * the MANIFEST.MF in the JAR file this class belongs to. 
+	 * @param name The name of the main attribute
+	 * @return The value of the main attribute or <code>null</code>, if no such value exists.
+	 * @throws IOException, if an error occurs while reading the MANIFEST.MF file.
+	 */
+	public static String getMainAttribute( MainAttribute att ) throws IOException {
+		if ( cachedManifest == null ) {
+			// attempt to read manifest
+			cachedManifest = read( Version.class );
+		}
+		
+		String result = null;
+		if ( cachedManifest != null ) {
+			result = cachedManifest.getMainAttributes().getValue( att.getName() );
+		}
+		return result;
+	}
 
 } // Version
