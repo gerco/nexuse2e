2110b2a07c6b94084719f9f6d241f972f2a1d38b
j_reese <j_reese@0f8715a3-b92c-0410-9248-d6441137b268>
2013-04-24 16:44:18 +0000

    Added configurable locale
    git-svn-id: http://nexuse2e-server.svn.sourceforge.net/svnroot/nexuse2e-server/trunk@1266 0f8715a3-b92c-0410-9248-d6441137b268

--- nexuse2e-core/src/main/java/org/nexuse2e/backend/pipelets/FreemarkerBackendPipelet.java
+++ nexuse2e-core/src/main/java/org/nexuse2e/backend/pipelets/FreemarkerBackendPipelet.java
@@ -24,12 +24,18 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
+import org.apache.commons.lang.LocaleUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.nexuse2e.NexusException;
 import org.nexuse2e.configuration.Constants.ParameterType;
+import org.nexuse2e.configuration.ListParameter;
 import org.nexuse2e.configuration.ParameterDescriptor;
 import org.nexuse2e.messaging.AbstractPipelet;
 import org.nexuse2e.messaging.MessageContext;
@@ -65,6 +71,7 @@ public class FreemarkerBackendPipelet extends AbstractPipelet {
     public static final String USE_EXTERNAL_TEMPLATE_PARAM_NAME = "externalTemplate";
     public static final String TEMPLATE_PARAM_NAME = "template";
     public static final String TEMPLATE_LOCATION_PARAM_NAME = "templateLocation";
+    public static final String LOCALE_PARAM_NAME = "locale";
     
     /**
      * Default constructor.
@@ -74,8 +81,23 @@ public class FreemarkerBackendPipelet extends AbstractPipelet {
                 "Use template in file system instead of the template text box below.", false));
         parameterMap.put(TEMPLATE_LOCATION_PARAM_NAME, new ParameterDescriptor(ParameterType.STRING, "Template Path",
                 "The file system path to the template. Ignored if external template is disabled.", ""));
+        ListParameter localeDropdown = new ListParameter();
+        // sort locales
+        List<LocaleAndDisplayName> l = new ArrayList<LocaleAndDisplayName>();
+        for (Locale loc : Locale.getAvailableLocales()) {
+            l.add(new LocaleAndDisplayName(loc));
+        }
+        Collections.sort(l);
+        
+        localeDropdown.addElement("Default", "");
+        for (LocaleAndDisplayName loc : l) {
+            localeDropdown.addElement(loc.locale.getDisplayName(), loc.locale.toString());
+        }
+        parameterMap.put(LOCALE_PARAM_NAME, new ParameterDescriptor(
+                ParameterType.LIST, "Locale", "The locale to be used for template processing", localeDropdown));
         parameterMap.put(TEMPLATE_PARAM_NAME, new ParameterDescriptor(ParameterType.TEXT, "Template",
                 "The template markup. Ignored if external template is enabled.", ""));
+
     }
     
     @Override
@@ -94,6 +116,12 @@ public class FreemarkerBackendPipelet extends AbstractPipelet {
                 LOG.debug("Processing inline freemarker template");
             }
             Configuration configuration = new Configuration(); // use defaults
+            Locale locale = Locale.getDefault();
+            ListParameter localeParam = getParameter(LOCALE_PARAM_NAME);
+            if (!StringUtils.isBlank(localeParam.getSelectedValue())) {
+                locale = LocaleUtils.toLocale(localeParam.getSelectedValue());
+            }
+            configuration.setLocale(locale);
 
             Template template = new Template(templateLocation, templateReader, configuration);
 
@@ -132,6 +160,24 @@ public class FreemarkerBackendPipelet extends AbstractPipelet {
             throw new NexusException(e);
         } catch (TemplateException e) {
             throw new NexusException(e);
+        }
+    }
+    
+    class LocaleAndDisplayName implements Comparable<LocaleAndDisplayName> {
+        Locale locale;
+        
+        public LocaleAndDisplayName(Locale locale) {
+            this.locale = locale;
+        }
+        
+        @Override
+        public int compareTo(LocaleAndDisplayName o) {
+            if (locale == null) {
+                return -1;
+            } else if (o.locale == null) {
+                return 1;
+            }
+            return locale.getDisplayName().compareTo(o.locale.getDisplayName());
         }
     }
 }
