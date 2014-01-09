816818852e9a14c0caa420040e5b68dd065e74fc
GEsch <guido.esch@direkt-gruppe.de>
2014-01-09 09:47:20 +0100

    tomcat integration test "removed"

--- nexuse2e-core/src/test/java/org/nexuse2e/test/IntegrationTest.java
+++ nexuse2e-core/src/test/java/org/nexuse2e/test/IntegrationTest.java
@@ -11,48 +11,48 @@ import org.junit.Test;
 
 public class IntegrationTest {
 
-	/** The tomcat instance. */
-	private Tomcat mTomcat;
-	/** The temporary directory in which Tomcat and the app are deployed. */
-	private String mWorkingDir = "c:\\temp_deployment"; //System.getProperty("java.io.tmpdir");
-	
-	
-	@Before
-	public void setup() throws Throwable {
-	  mTomcat = new Tomcat();
-	  mTomcat.setPort(0);
-	  mTomcat.setBaseDir(mWorkingDir);
-	  mTomcat.getHost().setAppBase(mWorkingDir);
-	  mTomcat.getHost().setAutoDeploy(true);
-	  mTomcat.getHost().setDeployOnStartup(true);
-	  
-	  String contextPath = "/NEXUSe2e";
-	  File webApp = new File(mWorkingDir, "NEXUSe2e");
-	  File oldWebApp = new File(webApp.getAbsolutePath());
-	  //FileUtils.deleteDirectory(oldWebApp);
-	   
-	  mTomcat.addWebapp("/NEXUSe2e", "NEXUSe2e");
-	  
-	  mTomcat.start();
-
-	}
-
-	
-	@After
-	public final void teardown() throws Throwable {
-	  if (mTomcat.getServer() != null
-	            && mTomcat.getServer().getState() != LifecycleState.DESTROYED) {
-	        if (mTomcat.getServer().getState() != LifecycleState.STOPPED) {
-	              mTomcat.stop();
-	        }
-	        mTomcat.destroy();
-	    }
-	}
-	
-	@Test
-	public void test1() {
-		int port = mTomcat.getConnector().getLocalPort();
-		System.out.println("port: "+port);
-		
-	}
+//	/** The tomcat instance. */
+//	private Tomcat mTomcat;
+//	/** The temporary directory in which Tomcat and the app are deployed. */
+//	private String mWorkingDir = "c:\\temp_deployment"; //System.getProperty("java.io.tmpdir");
+//	
+//	
+//	@Before
+//	public void setup() throws Throwable {
+//	  mTomcat = new Tomcat();
+//	  mTomcat.setPort(0);
+//	  mTomcat.setBaseDir(mWorkingDir);
+//	  mTomcat.getHost().setAppBase(mWorkingDir);
+//	  mTomcat.getHost().setAutoDeploy(true);
+//	  mTomcat.getHost().setDeployOnStartup(true);
+//	  
+//	  String contextPath = "/NEXUSe2e";
+//	  File webApp = new File(mWorkingDir, "NEXUSe2e");
+//	  File oldWebApp = new File(webApp.getAbsolutePath());
+//	  //FileUtils.deleteDirectory(oldWebApp);
+//	   
+//	  mTomcat.addWebapp("/NEXUSe2e", "NEXUSe2e");
+//	  
+//	  mTomcat.start();
+//
+//	}
+//
+//	
+//	@After
+//	public final void teardown() throws Throwable {
+//	  if (mTomcat.getServer() != null
+//	            && mTomcat.getServer().getState() != LifecycleState.DESTROYED) {
+//	        if (mTomcat.getServer().getState() != LifecycleState.STOPPED) {
+//	              mTomcat.stop();
+//	        }
+//	        mTomcat.destroy();
+//	    }
+//	}
+//	
+//	@Test
+//	public void test1() {
+//		int port = mTomcat.getConnector().getLocalPort();
+//		System.out.println("port: "+port);
+//		
+//	}
 }
