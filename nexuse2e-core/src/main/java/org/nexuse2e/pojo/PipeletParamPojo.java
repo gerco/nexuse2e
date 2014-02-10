f084d42e7c5cd4a8554d3496dfdbbfea7570f87b
JJerke <jascha.jerke@direkt-gruppe.de>
2014-02-10 18:52:51 +0100

    ! Fix missing Transient-annotations

--- nexuse2e-core/src/main/java/org/nexuse2e/pojo/PipeletParamPojo.java
+++ nexuse2e-core/src/main/java/org/nexuse2e/pojo/PipeletParamPojo.java
@@ -36,6 +36,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -94,6 +95,7 @@ public class PipeletParamPojo implements NEXUSe2ePojo {
     private int                 sequenceNumber;
 
     // non-persistent fields
+    @Transient
     private transient ParameterDescriptor parameterDescriptor;
 
     // Constructors
