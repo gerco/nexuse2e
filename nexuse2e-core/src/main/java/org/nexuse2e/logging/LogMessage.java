d84600396dfd2e4b729fe05572a35e04b26e4919
JJerke <jascha.jerke@direkt-gruppe.de>
2016-02-23 14:01:43 +0100

    Added additional null-checks to make sure generated LogMessages can never attempt to write null-values to the nx_message_log table.
    This relates to [NEXUS-613]

--- nexuse2e-core/src/main/java/org/nexuse2e/logging/LogMessage.java
+++ nexuse2e-core/src/main/java/org/nexuse2e/logging/LogMessage.java
@@ -1,45 +1,44 @@
 /**
- *  NEXUSe2e Business Messaging Open Source
- *  Copyright 2000-2009, Tamgroup and X-ioma GmbH
- *
- *  This is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation version 2.1 of
- *  the License.
- *
- *  This software is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this software; if not, write to the Free
- *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- *  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * NEXUSe2e Business Messaging Open Source
+ * Copyright 2000-2009, Tamgroup and X-ioma GmbH
+ * <p/>
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation version 2.1 of
+ * the License.
+ * <p/>
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 package org.nexuse2e.logging;
 
-import java.io.Serializable;
 
 import org.nexuse2e.messaging.MessageContext;
 import org.nexuse2e.pojo.MessagePojo;
 
+import java.io.Serializable;
+
+
 /**
- * @author mbreilmann,gesch
- * 
+ * @author mbreilmann, gesch
  */
 public class LogMessage implements Serializable {
 
-	private static final long serialVersionUID = 7284169389821283301L;
+    private static final long serialVersionUID = 7284169389821283301L;
 
-	private String description = null;
-	private String conversationId = "unknown";
-	private String messageId = "unknown";
-	private Throwable throwable;
-	private String choreograhyId ="unknown";
-    private String actionId = "unknown";
-
-    
+    private String description    = null;
+    private String conversationId = "unknown";
+    private String messageId      = "unknown";
+    private String choreograhyId  = "unknown";
+    private String actionId       = "unknown";
+    private Throwable throwable;
 
     /**
      * @param description
@@ -48,18 +47,18 @@ public class LogMessage implements Serializable {
      */
     public LogMessage(String description, String conversationId, String messageId, Throwable t) {
 
-    	 if(description != null) {
-         	this.description = description;
-         }
-         if(conversationId != null) {
-             this.conversationId = conversationId;
-         }
-         if(messageId != null) {
-             this.messageId = messageId;
-         }
-         if(messageId != null) {
-    	    this.throwable = t;
-         }
+        if (description != null) {
+            this.description = description;
+        }
+        if (conversationId != null) {
+            this.conversationId = conversationId;
+        }
+        if (messageId != null) {
+            this.messageId = messageId;
+        }
+        if (messageId != null) {
+            this.throwable = t;
+        }
     }
 
     /**
@@ -71,18 +70,22 @@ public class LogMessage implements Serializable {
 
         this.description = description;
         if (messagePojo != null) {
-        	if(messagePojo.getAction() != null) {
-        		this.actionId = messagePojo.getAction().getName();
-        	}
-            if (messagePojo.getConversation() != null) {
-            	if(messagePojo.getConversation().getChoreography() != null) {
-            		this.choreograhyId = messagePojo.getConversation().getChoreography().getName();
-            	}
-                this.conversationId = messagePojo.getConversation().getConversationId();
+            if (messagePojo.getAction() != null && messagePojo.getAction().getName() != null) {
+                this.actionId = messagePojo.getAction().getName();
             }
-            this.messageId = messagePojo.getMessageId();
+            if (messagePojo.getConversation() != null) {
+                if (messagePojo.getConversation().getChoreography() != null && messagePojo.getConversation().getChoreography().getName() != null) {
+                    this.choreograhyId = messagePojo.getConversation().getChoreography().getName();
+                }
+                if (messagePojo.getConversation().getConversationId() != null) {
+                    this.conversationId = messagePojo.getConversation().getConversationId();
+                }
+            }
+            if (messagePojo.getMessageId() != null) {
+                this.messageId = messagePojo.getMessageId();
+            }
         }
-        if(t != null) {
+        if (t != null) {
             this.throwable = t;
         }
     }
@@ -91,9 +94,9 @@ public class LogMessage implements Serializable {
      * @param description
      */
     public LogMessage(String description, Throwable t) {
-    	this(description,null,null,t);
+        this(description, null, null, t);
     }
-    
+
     /**
      * @param description
      * @param messageContext
@@ -106,7 +109,7 @@ public class LogMessage implements Serializable {
      * @param description
      */
     public LogMessage(String description) {
-    	this(description,null,null,null);
+        this(description, null, null, null);
     }
 
     /**
@@ -115,7 +118,7 @@ public class LogMessage implements Serializable {
      * @param messageId
      */
     public LogMessage(String description, String conversationId, String messageId) {
-    	this(description,conversationId,messageId,null);
+        this(description, conversationId, messageId, null);
     }
 
     /**
@@ -123,7 +126,7 @@ public class LogMessage implements Serializable {
      * @param messagePojo
      */
     public LogMessage(String description, MessagePojo messagePojo) {
-    	this(description,messagePojo,null);
+        this(description, messagePojo, null);
     }
 
     /**
@@ -134,113 +137,117 @@ public class LogMessage implements Serializable {
         this(description, (messageContext != null ? messageContext.getMessagePojo() : null));
     }
 
-	/**
-	 * @return
-	 */
-	public String getConversationId() {
+    /**
+     * @return
+     */
+    public String getConversationId() {
 
-		return conversationId;
-	}
+        return conversationId;
+    }
 
-	/**
-	 * @param conversationId
-	 */
-	public void setConversationId(String conversationId) {
+    /**
+     * @param conversationId
+     */
+    public void setConversationId(String conversationId) {
 
-		this.conversationId = conversationId;
-	}
+        this.conversationId = conversationId;
+    }
 
-	/**
-	 * @return
-	 */
-	public String getDescription() {
+    /**
+     * @return
+     */
+    public String getDescription() {
 
-		return description;
-	}
+        return description;
+    }
 
-	/**
-	 * @param description
-	 */
-	public void setDescription(String description) {
+    /**
+     * @param description
+     */
+    public void setDescription(String description) {
 
-		this.description = description;
-	}
+        this.description = description;
+    }
 
-	/**
-	 * @return
-	 */
-	public String getMessageId() {
+    /**
+     * @return
+     */
+    public String getMessageId() {
 
-		return messageId;
-	}
+        return messageId;
+    }
 
-	/**
-	 * @param messageId
-	 */
-	public void setMessageId(String messageId) {
+    /**
+     * @param messageId
+     */
+    public void setMessageId(String messageId) {
 
-		this.messageId = messageId;
-	}
+        this.messageId = messageId;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
 
-		return toString(true);
-	}
+        return toString(true);
+    }
 
-	/**
-	 * Gets the <code>Throwable</code> associated with this <code>LogMessage</code>.
-	 * @return The <code>Throwable</code>, or <code>null</code> if no <code>Throwable</code> is associated with this
-	 * <code>LogMessage</code>.
-	 */
-	public Throwable getThrowable() {
-	    return throwable;
-	}
-	
-	/**
-	 * Extracts an error message from this <code>LogMessage</code>'s <code>Throwable</code>.
-	 * @return An error message, or <code>null</code> if no <code>Throwable</code> is associated with this
-	 * <code>LogMessage</code>.
-	 */
-	protected String getErrorMessage() {
-	    
-	    Throwable t = throwable;
-	    while (t != null) {
-	        if (t.getCause() != null) {
-	            t = t.getCause();
-	        } else {
-	            break;
-	        }
-	    }
-	    if (t != null) {
-	        if (t.getMessage() != null && t.getMessage().length() > 0) {
-	            return t.getMessage();
-	        } else {
-	            return t.getClass().getSimpleName();
-	        }
-	    }
-	    return null;
-	}
-	
-	/**
-	 * by default toString prepends message and conversation id. Use full=false
-	 * to suppress unnecessary ids.
-	 * 
-	 * @param full
-	 * @return
-	 */
-	public String toString(boolean full) {
+    /**
+     * Gets the <code>Throwable</code> associated with this <code>LogMessage</code>.
+     *
+     * @return The <code>Throwable</code>, or <code>null</code> if no <code>Throwable</code> is associated with this
+     * <code>LogMessage</code>.
+     */
+    public Throwable getThrowable() {
+        return throwable;
+    }
 
-	    String errorMessage = getErrorMessage();
-		if (full) {
-			return conversationId + "/" + messageId + " ("+choreograhyId+"->"+actionId+"): " + description + (errorMessage != null ? ": " + errorMessage : "");
-		} else {
-			return description + (errorMessage != null ? ": " + errorMessage : "");
-		}
-	}
+    /**
+     * Extracts an error message from this <code>LogMessage</code>'s <code>Throwable</code>.
+     *
+     * @return An error message, or <code>null</code> if no <code>Throwable</code> is associated with this
+     * <code>LogMessage</code>.
+     */
+    protected String getErrorMessage() {
+
+        Throwable t = throwable;
+        while (t != null) {
+            if (t.getCause() != null) {
+                t = t.getCause();
+            } else {
+                break;
+            }
+        }
+        if (t != null) {
+            if (t.getMessage() != null && t.getMessage().length() > 0) {
+                return t.getMessage();
+            } else {
+                return t.getClass().getSimpleName();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * by default toString prepends message and conversation id. Use full=false
+     * to suppress unnecessary ids.
+     *
+     * @param full
+     * @return
+     */
+    public String toString(boolean full) {
+
+        String errorMessage = getErrorMessage();
+        if (full) {
+            return conversationId + "/" + messageId + " (" + choreograhyId + "->" + actionId + "): " + description + (errorMessage != null ?
+                ": " + errorMessage :
+                "");
+        } else {
+            return description + (errorMessage != null ? ": " + errorMessage : "");
+        }
+    }
 }
