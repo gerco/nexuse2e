2142ec008e566ea10fbb67b373e00b0f15a2b654
JJerke <jascha.jerke@direkt-gruppe.de>
2016-05-12 08:51:27 +0200

    Added extra CRLF in HttpMessagePacker which was lost during earlier binary changes.
    This relates to NEXUS-636

--- nexuse2e-core/src/main/java/org/nexuse2e/messaging/ebxml/HTTPMessagePackager.java
+++ nexuse2e-core/src/main/java/org/nexuse2e/messaging/ebxml/HTTPMessagePackager.java
@@ -193,6 +193,7 @@ public class HTTPMessagePackager extends AbstractPipelet {
                     baos.write(payloadPojo.getPayloadData());
                 }
 
+                msgBuffer.append(CRLF);
             }
         }
 
